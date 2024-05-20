29c827ede32c0c8d8a2021f0cddef301b2499246
givoxxs <toanyogame@gmail.com>
2024-05-21 01:20:36 +0700

    install morgan, template engine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const exphbs  = require('express-handlebars').create;
+const path = require('path');
+// import { engine } from 'express-handlebars';
+//.create;
+const app = express();
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Temple engine
+app.engine('hbs', exphbs({
+  // chang .handlebars to .hbs
+  extname: '.hbs'
+}).engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.render('home');
+});
 
-app.listen(3000)
+app.get('/news', function (req, res) {
+  res.render('news');
+});
+
+app.listen(3000);
