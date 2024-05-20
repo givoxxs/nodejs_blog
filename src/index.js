5b70e5cdff6cc199fbae0c12ce382f260bf860c0
givoxxs <toanyogame@gmail.com>
2024-05-21 01:51:24 +0700

    Static files & SCSS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs  = require('express-handlebars').create;
 const path = require('path');
-// import { engine } from 'express-handlebars';
-//.create;
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
 app.use(morgan('combined'));
